ce2d10e4eb2a15a980ae7a9c1cef122047ba15f9
mohamedessamkamal50 <mohamedessamkamal50@gmail.com>
2020-10-31 18:32:14 +0200

    Updates

---
+++ precache-manifest.c4c296fa3f65fbd17fc3fb373a49db23.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d500d292755bd46272c8a1926779ed",
+    "url": "/objectiv/index.html"
+  },
+  {
+    "revision": "b2681b36151596daf2a7",
+    "url": "/objectiv/static/css/main.06911859.chunk.css"
+  },
+  {
+    "revision": "0463e2bb0551eaed7b30",
+    "url": "/objectiv/static/js/2.c3bf7f91.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/objectiv/static/js/2.c3bf7f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2681b36151596daf2a7",
+    "url": "/objectiv/static/js/main.2ea6b5ee.chunk.js"
+  },
+  {
+    "revision": "eaaf292e386501107ac7",
+    "url": "/objectiv/static/js/runtime-main.e90b2954.js"
+  },
+  {
+    "revision": "676357497597f7ad2224f2cff60917fd",
+    "url": "/objectiv/static/media/svg-1.67635749.svg"
+  },
+  {
+    "revision": "e237a62a432b2653340ddda8b3435630",
+    "url": "/objectiv/static/media/svg-2.e237a62a.svg"
+  },
+  {
+    "revision": "02cce8cf0633e0ad288589c045685527",
+    "url": "/objectiv/static/media/svg-3.02cce8cf.svg"
+  },
+  {
+    "revision": "844075333b429f96651f15cf5fd3ecf2",
+    "url": "/objectiv/static/media/svg-4.84407533.svg"
+  },
+  {
+    "revision": "ed44dc5d1d3df4597fbdd91513f53382",
+    "url": "/objectiv/static/media/video.ed44dc5d.mp4"
+  }
+]);
